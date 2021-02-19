4b2f974e122d26b884cf0f06f55178a86a07072d
DavidRS <dr16622@bristol.ac.uk>
2021-02-19 12:26:36 +0000

    New module that calculates in the command line the mode, average, and median of the inputs.

---
+++ newexample.py
@@ -0,0 +1,41 @@
+import sys
+import statistics
+from collections import Counter
+
+def average(n):
+    aver = sum(n)/len(n)
+    return aver
+
+def mode(n):
+
+    return Counter(n).most_common(1)[0][0]
+
+
+def median(n):
+    return statistics.median(n)
+
+def main(args):
+    NUMBERS = []
+    for x in args:
+        try:
+            NUMBERS.append(float(x))
+        except:
+            pass
+    
+    if '--average' in args:
+        print(f'Average: {average(NUMBERS)}')
+
+    if '--mode' in args:
+        print(f'Mode: {mode(NUMBERS)}')
+
+    if '--median' in args:
+        print(f'Median: {median(NUMBERS)}')
+
+    if len (NUMBERS) == len(args):
+        print(f'Average: {average(NUMBERS)}')
+        print(f'Mode: {mode(NUMBERS)}')
+        print(f'Median: {median(NUMBERS)}')
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
